6571f3be98f9b86de08dac7f1ee7f60aa33be150
Robert Jennings <roje@noc.ac.uk>
2026-01-27 16:26:39 +0000

    lint

--- lib/widgets/top_navigation.dart
+++ lib/widgets/top_navigation.dart
@@ -3,12 +3,11 @@ import 'package:smart_tags/widgets/common/user_icon_button.dart';
 
 /// A custom AppBar widget for the Smart Tags app.
 class TopNavigation extends AppBar {
-  /// Creates a [TopNavigation] widget with an optional [title], [leading] widget, and [actions].
-  /// [actions] defaults to a user icon button if not provided.
-  TopNavigation({super.key, Widget? title, Widget? leading, List<Widget>? actions})
+  /// Creates a [TopNavigation] widget with an optional [title], [leading] widget.
+  /// [leading] defaults to a user icon button if not provided.
+  TopNavigation({super.key, Widget? title, Widget? leading, super.actions})
     : super(
         title: title ?? const Text('Smart Tags'),
         leading: leading ?? const UserIconButton(),
-        actions: actions,
       );
 }
