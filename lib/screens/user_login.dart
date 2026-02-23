40621e6eb22e3927a433ea120ae4e85f96b26ba8
ewetchy <elisabeth.wetchy@noc.ac.uk>
2026-02-23 12:03:04 +0000

    Remove password length validation

--- lib/screens/user_login.dart
+++ lib/screens/user_login.dart
@@ -141,9 +141,6 @@ class _UserLoginState extends ConsumerState<UserLoginScreen> {
                       if (value == null || value.isEmpty) {
                         return 'Please enter your password';
                       }
-                      if (value.length < 6) {
-                        return 'Password must be at least 6 characters';
-                      }
                       return null;
                     },
                   ),
